75d00327633e6fb9e0f21958854d2a2d2ae6702d
Ward Nakchbandi (Cosmic Fusion) <hotrod.master@hotmail.com>
2023-05-25 01:28:54 +0300

    wtf

--- main.sh
+++ main.sh
@@ -7,11 +7,8 @@ wget -q -O - https://ppa.pika-os.com/key.gpg | sudo apt-key add -
 add-apt-repository https://ppa.pika-os.com
 add-apt-repository ppa:pikaos/pika
 add-apt-repository ppa:kubuntu-ppa/backports
-apt install pika-sources.deb --yes --option Acquire::Retries=5 --option Acquire::http::Timeout=100 --option Dpkg::Options::="--force-confnew"
 # Clone Upstream
-mkdir -p ./src-pkg-name
-cp -rvf ./debian ./src-pkg-name/
-cd ./src-pkg-name/
+cd ./pika-refind-theme
 
 # Get build deps
 ln -fs /usr/share/zoneinfo/America/New_York /etc/localtime
